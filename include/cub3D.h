1164295e7500b7c6f2de6ff67cdb19f0f0753e11
Blankxx420 <briceguichou.dev@gmail.com>
2024-12-04 08:17:48 +0100

    ok

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: brguicho <brguicho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 18:30:03 by tbraud            #+#    #+#             */
-/*   Updated: 2024/12/04 08:12:35 by brguicho         ###   ########.fr       */
+/*   Updated: 2024/12/04 08:17:35 by brguicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,34 +56,6 @@ typedef struct s_raycast
 	int		draw_end;
 }				t_raycast;
 
-# define S_WIDTH 600
-# define S_HEIGHT 900
-
-typedef struct s_raycast
-{
-	double 	dir_x;
-	double 	dir_y;
-	double 	plane_y;
-	double 	plane_x;
-	double 	sidedist_x;
-	double 	sidedist_y;
-	double	camera_x;
-	double	deltadist_x;
-	double	deltadist_y;
-	double 	perpwalldist;
-	double	ray_x;
-	double	ray_y;
-	int		side_wall;
-	int		hit_wall;
-	int		step_x;
-	int		step_y;
-	int 	map_x;
-	int 	map_y;
-	int		line_height;
-	int		draw_start;
-	int		draw_end;
-}				t_raycast;
-
 typedef struct s_data
 {
 	char	**map;
@@ -99,14 +71,7 @@ typedef struct s_data
 	char	*WE;
 	char	*EA;
 	t_raycast *raycast;
-	t_raycast *raycast;
 }			t_data;
-
-
-
-
-
-
 
 //|----parsing----|
 void		ft_init_data(t_data *data, char *argv); // remplir la struct en lisant le doc
